73b3cb0a2f38606bcb45ce31e9e335878de83a60
Myzaxx89 <98712213+Myzaxx89@users.noreply.github.com>
2022-08-15 22:53:06 +0200

    Add files via upload

---
+++ ritchpresence/config.lua
@@ -0,0 +1,24 @@
+Config = {}
+
+Config.ClientID = 961722779223339058
+
+Config.PlayerCount = 10
+
+Config.PlayerText = "Joueurs" -- Player text. Example Players 10/32
+
+Config.ResourceTimer = 5 -- Time in witch resource updates in seconds
+
+Config.UseESXIdentity = true -- Uses ESX Identity name not steam name
+
+Config.RichPresence = 'ID: %s | %s | %s %s/%s'
+
+Config.UseJobs = true
+
+Config.Buttons = { -- Your buttons
+
+    {index = 0,name = 'Discord',url = 'https://discord.gg/KSCnux6Uwu'},
+
+    {index = 1,name = 'Connexion',url = 'pas dispo'}
+
+}
+
---
+++ ritchpresence/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5' 
+
+
+author 'Boost#4383'
+description 'Discord rich presence with ESX compatibility'
+version '1.0.5'
+
+shared_script 'config.lua'
+client_script {
+	'@ClippyGeek/config.lua',
+	'@ClippyGeek/client/cl_anticheat.lua',
+	'client/main.lua'
+}
+
+server_script{
+	'@mysql-async/lib/MySQL.lua',
+	'server/main.lua'
+}
